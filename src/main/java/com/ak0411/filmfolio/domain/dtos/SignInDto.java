b43cb51d27888aa85243272da03624dc54868697
Anawil Karavek <akaravek@hotmail.com>
2024-05-09 22:00:55 +0700

    updated SignInDto

--- src/main/java/com/ak0411/filmfolio/domain/dtos/SignInDto.java
+++ src/main/java/com/ak0411/filmfolio/domain/dtos/SignInDto.java
@@ -1,16 +1,12 @@
 package com.ak0411.filmfolio.domain.dtos;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record SignInDto(
         @NotNull(message = "Username cannot be empty")
-        @Size(min = 3, max = 20, message = "Username should be between 3 - 20 characters long")
         String username,
 
         @NotNull(message = "Password cannot be empty")
-        @Min(value = 5, message = "Password should be at least 5 characters long")
         String password
 ) {
 }
